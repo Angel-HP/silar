108c051ab7ddf00b9fd988bcdfa55f016633065c
Klvst3r <klvst3r@yahoo.com>
2019-12-27 13:37:24 -0600

    3.2.3. Crear la funcion de validacion de campos

---
+++ help/helper.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Este codigo valida los campos provenientes de formularios enviados por el metodo POST
+Lo que hace es limpiar los espacios vacios y evitando insercion de codigo SQL o JS
+definido en un afuncion que recibe un valor escrito desde el formulario
+ */
+
+function validate_field($field){
+	$field = trim($field);
+	$field = stripcslashes($field);
+	$field = htmlspecialchars($field);
+}
