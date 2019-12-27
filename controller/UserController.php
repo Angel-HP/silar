3ed3d8efe2b5f281da868085f9f1a7283ad530e0
Klvst3r <klvst3r@yahoo.com>
2019-12-27 13:10:30 -0600

    3.2.1. Crear metodo login del controlador USerController

--- controller/UserController.php
+++ controller/UserController.php
@@ -4,3 +4,21 @@
 //utilizarlo en la clase definida de acceso de usuario
 
 include '../data/UserDAO.php';
+class UserController{
+	//Se define el login con dos parametros, usuario y contraseña a traves de un objeto que enviara los datos
+	//para su validación
+	public static function login($user, $pass){
+		//Inizializamos un objeto para enviar valores (usuario y contraseña)
+		$obj_user = new User();
+
+		//*Enviamos el objeto User desde la aplicacion (formulario)
+		$obj_user->setUser($user);
+		$obj_user->setPass($pass);
+
+		/*La clase nos envia por metodo (login), los parametros al objeto (obj_user) los valores recibidos del
+			formulario (user. pass), nos devuelve el resultado de la operacion (true, false)
+		*/
+		return UserDAO::login($obj_user);
+	}
+
+}
--- index.php
+++ index.php
@@ -18,10 +18,10 @@
     <form class="form-signin" id="loginForm" action="inc/validaCode.php" method="POST" role="form">
       <img class="mb-4" src="assets/img/favicons/favicon.ico" alt="" width="72" height="72">
       <h1 class="h3 mb-3 font-weight-normal">Ingrese sus credenciales</h1>
-      <label for="inputUser" class="sr-only">Usuario</label>
-      <input type="text" id="inputUser" class="form-control" placeholder="Nombre de usuario" required autofocus>
-      <label for="inputPassword" class="sr-only">Password</label>
-      <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+      <label for="user" class="sr-only">Usuario</label>
+      <input type="text" id="user" class="form-control" placeholder="Nombre de usuario" name="user" required autofocus>
+      <label for="pass" class="sr-only">Password</label>
+      <input type="password" id="pass" name ="pass" class="form-control" placeholder="Password" required>
       <div class="checkbox mb-3">
  
       </div>
