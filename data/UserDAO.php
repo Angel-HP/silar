1d660d9d944d8b844ade07a87fb8bd81c96d4003
Klvst3r <klvst3r@yahoo.com>
2020-01-08 13:56:45 -0600

    Se corrigieron errores en el objeto del usuario

--- data/UserDAO.php
+++ data/UserDAO.php
@@ -11,36 +11,40 @@ class UserDAO extends Connect {
 
 	//Definir la conexion a la BD
 	private static function getConnection(){
-		$self::$cnx = Connect::conn();
+		self::$cnx = Connect::conn();
 
 	}
 
 	//Definir la desconexion a la BD
 	private static function disconnect(){
-		$self::$cnx = NULL;		
+		self::$cnx = NULL;	
+		
 	}
 
 	//Localizar datos en la BD del Usuario de Login
 	public static function login($user){
 		//Una consulta es generada por los campos seleccionado que seran enviados sin comprometer la seguridad del sistema, tal como datos visibles via HTML, Inyeccion de SQL o Js y CrosSiteScripting
+
+		$query = "SELECT id_user, id_priv, name, user_name, user_pass, user_email, id_status_user, user_position, online FROM users WHERE user_name = :user_name AND user_pass = :user_pass";
 		
-		$query = "SELECT id_user, id_priv, name, user, pass, email, id_status_user, position, online 
-				FROM users WHERE user = ':user' AND pass = ':pass'";
 
 		//Inizializar la conexion con la BD
-		$self::getConnection();
+		self::getConnection();
 
 		//Preparar la consulta parametrizada (Los parametros devueltos son user y pass desde el formulario)
-		$result = $self::$cnx->prepare($query);
+		$result = self::$cnx->prepare($query);
 
 		//Los parametros son enviados el usuario desde los marcadores respectivos (:user y :pass)
 		//desde el formulario y almacenados en el objeto $user y $pass
-		$user_bd  = $user->getUser();
-		$result->bindParam(":user",$user_bd);
+		$user_bd  = $user->getUser_name();
+		$result->bindParam(":user_name",$user_bd);
 
 		//el mismo valor del objeto user->password del usuario enviado hacia el marcador :pass
-		$pass_bd = $user->getPass();
-		$result->bindParam(":pass",$pass_bd);
+		$pass_bd = $user->getUser_pass();
+		$result->bindParam(":user_pass",$pass_bd);
+
+
+		
 
 		//Corremos o ejecutamos la consulta preparada con PDO
 		$result->execute();
@@ -50,22 +54,35 @@ class UserDAO extends Connect {
 		Contabilizamos el resultado de la consulta, comprobando que sea verdadero (1)
 		$result->rowCount() = 1 que es el valor devuelto, de valores encontrados en la ejecucion de la consulta
 		 */
+		
+			
+
 		if($result->rowCount() > 0){
+
 			//retornamos el valor hacia un arreglo
 			$data = $result->fetch();
 
-			if($data['user'] == $user->getUser() && $data['pass']==$user->$user->getPass()){
+
+
+
+			if($data['user_name'] == $user->getUser_name() && $data['user_pass'] == $user->getUser_pass()){
+
+				
 				//Verifico el estatus del usuario que ingreso
 				if($data['id_status_user'] <> 1){
 					$status = true;
 					return false;
 				}
 
+				
 				$login = true;
 				return $login;
+				
+				
 				//cerramos la conexion activa con la BD
-				$self::disconnect();
+				self::disconnect();
 			}
+			return true;
 
 		}
 		//En caso de que sea Negativo retornamos la bandera a negativo
@@ -73,32 +90,42 @@ class UserDAO extends Connect {
 		
 	}//Metodo Login
 
+	public static function logout(){
+		self::disconnect();
+	}
+
 	public static function getUser($user){
 
-		$query = "SELECT id_user, name, user, pass, email FROM users WHERE
-		user=':user' and pass=':pass'";
+		$query = "SELECT id_user, id_priv, name, user_name, user_pass, user_email FROM users WHERE
+		user_name = :user_name and user_pass = :user_pass";
 
-		self::getConection();
+		self::getConnection();
 
 		$result = self::$cnx->prepare($query);
 
 		$user_bd = $user->getUser_name();
-		$result->bindParam(":user", $user_bd);
+		$result->bindParam(":user_name", $user_bd);
 
 		$pass_bd = $user->getUser_pass();
-		$result->bindParam(":pass", $pass_bd);
+		$result->bindParam(":user_pass", $pass_bd);
 
 		$result->execute();
 
 		$data = $result->fetch();
+       
 
+		$user = new User();
 		//Para enviar la información a la validacion y visualizacion de datos del usuario autentificado
 		//lo cargaremos desde el objeto del usuario a traves de su instancia de objeto de la entidad usuarios.
 		$user->setId_user($data["id_user"]);
+		$user->setId_priv($data["id_priv"]);
 		$user->setName($data["name"]);
-		$user->setUser($data["user"]);
-		$user->setPass($data["pass"]);
-		$user->setEmail($data["user_email"]);
+		$user->setUser_name($data["user_name"]);
+		$user->setUser_pass($data["user_pass"]);
+		$user->setUser_email($data["user_email"]);
+
+		
+		self::disconnect();
 
 		//Retornamos los vaores al objeto User
 		return $user;
