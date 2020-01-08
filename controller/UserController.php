1286592508e4c4ef1dd262ca8aa9291f2659ac0b
Klvst3r <klvst3r@yahoo.com>
2020-01-08 13:54:34 -0600

    3.2.13. Metodo getUser en el UserController

--- controller/UserController.php
+++ controller/UserController.php
@@ -7,26 +7,39 @@ include '../data/UserDAO.php';
 class UserController{
 	//Se define el login con dos parametros, usuario y contraseña a traves de un objeto que enviara los datos
 	//para su validación
-	public static function login($user, $pass){
+	public static function login($user_name, $user_pass){
 		//Inizializamos un objeto para enviar valores (usuario y contraseña)
 		$obj_user = new User();
 
 		//*Enviamos el objeto User desde la aplicacion (formulario)
-		$obj_user->setUser($user);
-		$obj_user->setPass($pass);
+		$obj_user->setUser_name($user_name);
+		$obj_user->setUser_pass($user_pass);
+
 
 		/*La clase nos envia por metodo (login), los parametros al objeto (obj_user) los valores recibidos del
 			formulario (user. pass), nos devuelve el resultado de la operacion (true, false)
 		*/
 		return UserDAO::login($obj_user);
+		
 	}
-	public function getUser($user, $pass){
+
+	public static function logout(){
+		
+		return UserDAO::logout();
+	}
+
+	public function getUser($user_name, $user_pass){
 		$obj_user = new User();
 
-		$obj_user->getUser($user);
-		$obj_user->getUser($pass);
+		$obj_user->setUser_name($user_name);
+		$obj_user->setUser_pass($user_pass);
 
 		return UserDAO::getUser($obj_user);
 	}
+
+	/*public function changeIn($idUser){
+	}*/
+	/*public function changeOut($idUser){}
+	*/
 
 }
