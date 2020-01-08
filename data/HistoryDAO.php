5303bb0c4c2793b31464cb35a332a9077d59e1a8
Klvst3r <klvst3r@yahoo.com>
2020-01-08 13:49:39 -0600

    3.2.11 y 3.2.12 Se define el metodo updateHistory en el objeto HistoryDAO

--- data/HistoryDAO.php
+++ data/HistoryDAO.php
@@ -1,13 +1,13 @@
 <?php
-include '../inc/Connect.php';
+include '../inc/ConnectHistory.php';
 
 include '../model/History.php';
 
-class HistoryDAO extends Connect {
+class HistoryDAO extends ConnectHistory {
 	protected static $cnx;
 
 	private static function getConection(){
-			self::$cnx = Connect::connection();
+			self::$cnx = Connect::conn();
 		
 	}
 
@@ -16,7 +16,7 @@ class HistoryDAO extends Connect {
 	}
 
 	public static function regHistory($history){
-		$query = "INSERT INTO `history_access` (`id_history`, `ip`, `date_access`, `time_in`, `time_out`) VALUES (NULL, ':ip', ':date_access', ':time_in', ':time_out')";
+		$query = "INSERT INTO `history_access` (`id_history`, `ip`, `date_access`, `time_in`, `time_out`) VALUES (NULL, :ip, :date_access, :time_in, :time_out)";
 
 		//Fase 1. Preparacion de la consulta parametrizada
 		self::getConection();
@@ -37,20 +37,66 @@ class HistoryDAO extends Connect {
 
 		//Fase 2. Ejecutar consulta
 		if($result->execute()){
-			//return true;
-			echo "Insercion Exitosa";
-			//$lastId = $result->insert_id();
-			/*$temp_result = $result->fetch(PDO::FETCH_ASSOC);
-            return ( $temp_result ) ? $temp_result['last_value'] : false;
-            */
-           /* Haremos una seleccion del ultimo registro y lo enviamos a la tabla de Usuarios como una actualización */
-           //echo "Last ID: " . $lastId;
+			
 			
 			self::disconnect();
-			//return $lastId;
+			
 			return true;
 		}
 		return false;
 
 	}
-}
+
+	public static function updateHistory($user){
+		//Consultar ultimo acceso al sistema
+		$query = "SELECT id_history, ip, date_access, time_in, time_out FROM `history_access` ORDER BY `id_history` DESC LIMIT 1";
+
+
+
+		self::getConection();
+	
+		$result = self::$cnx->prepare($query);
+
+		if($result->execute()){
+
+		$data = $result->fetch();
+
+		//echo $data['id_history'];
+
+		$history = new History();
+
+		$history->setId_history($data["id_history"]);
+		$idHistory = $data["id_history"];
+
+		$history->setIp($data["ip"]);
+		$history->setDate_access($data["date_access"]);
+		$history->setTime_in($data["time_in"]);
+		$history->setTime_out($data["time_out"]);
+
+		//echo "Id History: " . $idHistory;
+
+		$update = "UPDATE `users` SET `id_history` = '$idHistory' WHERE `users`.`id_user` = :id_user";
+
+		//echo $update;
+
+		self::getConection();
+
+		$resultUpdate = self::$cnx->prepare($update);
+
+		//echo $update;
+
+		$id_user = $user->getId_history();
+		$resultUpdate->bindParam(":id_user", $id_user);
+
+		//echo $id_user;
+		$resultUpdate->execute();
+
+		self::disconnect();
+	return true;
+	}
+
+	return false;
+
+
+	}//metodo updateHistory
+}//Class History DAO
