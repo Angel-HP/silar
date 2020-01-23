fefbe342fc8351e01120ce452b24a2b700a97cbc
angelhp <angelhpjuli@gmail.com>
2020-01-23 12:50:03 -0600

    Template dev

---
+++ dev/indexdev.php
@@ -0,0 +1,23 @@
+
+<?php
+require "template/incdev/head.html";
+?>
+<?php
+require "template/incdev/sidebar.html";
+?>
+
+<div class="main-panel" id="main-panel">
+	
+<?php
+require "template/incdev/navbar.html";
+?>
+<?php
+require "template/incdev/content.html";
+?>
+</div>
+<?php
+//require "tempalte/inc/footer.html";
+
+require "template/incdev/core-js.html";
+?>      
+</body></html>
